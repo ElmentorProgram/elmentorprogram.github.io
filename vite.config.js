822e789bf4ebbaaddf7e32d6f870fb99507d8889
Rehab Ragab <67760628+RehabAbotalep@users.noreply.github.com>
2025-06-13 11:01:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/elmentor-landing-page/', // Set base for GitHub Pages - correct for project repositories
+  base: '/', // Set base for GitHub Pages - correct for project repositories
   plugins: [react()],
   // Add development server configuration
   server: {
