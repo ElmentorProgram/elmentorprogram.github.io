b1423150be0addb445a28cb195bec0540a755b51
Rehab Ragab <67760628+RehabAbotalep@users.noreply.github.com>
2025-07-04 14:24:23 +0000

    Deploy production build to root

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    server: {
-      port: 3000,
-      open: true
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: true
-    }
-  }
-  
-  // Set the base path depending on the mode
-  if (command === 'build' || command === 'serve') {
-    // For production build and preview
-    config.base = '/elmentor-landing-page-clean/'
-  } else {
-    // For development
-    config.base = '/'
-  }
-  
-  return config
-})
+export default defineConfig(() => ({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  base: '/'
+}))
